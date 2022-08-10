ca3fda1484a048fc77ac86fff0981c4a61a6001f
Andrew Nguyen <nguyendrew20@gmail.com>
2022-08-09 23:35:51 -0700

    Initial commit

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.put('/image', (req, res) => {image.handleImage(req,res,db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req,res)})
 
 
-
+//Listening port
 app.listen(process.env.PORT || 3000, () => {
     console.log(`app is running on port ${process.env.PORT}`);
 })
