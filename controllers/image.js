bf3d48020e0f863fe4f370d45ee27cffc22862f3
Andrew Nguyen <nguyendrew20@gmail.com>
2023-02-09 18:46:56 -0800

    updated api caller

--- controllers/image.js
+++ controllers/image.js
@@ -8,12 +8,24 @@ const app = new Clarifai.App({
 
 
 const handleApiCall = (req, res) => {
+    // app.models
+    //     .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    //     .then(data => {
+    //         res.json(data);
+    //     })
+    //     .catch(err => res.status(400).json('unable to work with API'))
     app.models
-        .predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => res.status(400).json('unable to work with API'))
+    .predict(
+      {
+        id: 'face-detection',
+        name: 'face-detection',
+        version: '6dc7e46bc9124c5c8824be4822abe105',
+        type: 'visual-detector',
+      }, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
 }
 
 // '53e1df302c079b3db8a0a36033ed2d15',
